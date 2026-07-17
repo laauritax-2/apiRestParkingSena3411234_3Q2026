0e16e8c80a689f38bf387b7fe9efd069e66fd53e
lauritax <melissagomezal22@gmail.com>
2026-07-16 22:13:33 -0500

    v0.3_interfacesPaqueteRepository_lauritax_160726

---
+++ src/main/java/com/sena/parqueadero/repository/IRegistroRepository.java
@@ -0,0 +1,19 @@
+package com.sena.parqueadero.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sena.parqueadero.model.Registro;
+import com.sena.parqueadero.model.TipoVehiculo;
+
+public interface IRegistroRepository extends JpaRepository<Registro, Long> {
+	Optional<Registro> findByVehiculoPlacaAndActivoTrue(String placa);
+
+	List<Registro> findByActivoTrue();
+
+	boolean existsByVehiculoIdVehiculo(Long VehiculoId);
+
+	boolean existsByVehiculoTipo(TipoVehiculo TipoVehiculo);
+}
